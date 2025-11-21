ac13dc1f9173fe4d8523d7f3faa50742bb726c5a
akbarxyanto <dikipt4@gmail.com>
2025-11-21 19:34:22 +0700

    Update access message for Nest menu

--- installprotect5.sh
+++ installprotect5.sh
@@ -56,7 +56,7 @@ class NestController extends Controller
         // 🔒 Proteksi: hanya user ID 1 (superadmin) yang bisa akses menu Nest
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '🚫 Akses ditolak! Hanya admin utama (ID 1) yang bisa membuka menu Nests. ©MalzXyz Protect t.me/malzxyz v1.3');
+            abort(403, '🚫 Akses ditolak! Hanya admin utama (ID 1) yang bisa membuka menu Nests. © AkbarDev Protect t.me/Akbarn8 v1.3');
         }
 
         return $this->view->make('admin.nests.index', [
